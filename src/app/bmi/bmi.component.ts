7dc653521dcd318520b107661b7ddb88dca0a6c0
Blakypok <saroj.s.2002.official@gmail.com>
2025-01-27 16:29:11 +0530

    update

--- src/app/bmi/bmi.component.ts
+++ src/app/bmi/bmi.component.ts
@@ -6,25 +6,31 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './bmi.component.html',
-  styleUrl: './bmi.component.css'
+  styleUrls: ['./bmi.component.css']
 })
 export class BmiComponent {
-  weight:number=0
-  height:number=0
-  bmi:string=""
+  weight: number = 0;
+  height: number = 0;
+  bmi: number | null = null;
+  category: string = '';
 
   calculateBMI(): void {
-    if(this.weight&&this.height){
-      let meter = this.height/100;
-      let metsq =  meter**2;
-      let result = this.weight/metsq
-      let resultround = Math.round(result)
-      this.bmi = resultround.toString();
-      
-    }
-    else{
-      alert("fill the feld")
-    }
+    if (this.weight && this.height) {
+      const meter = this.height / 100;
+      const bmiValue = this.weight / (meter * meter);
+      this.bmi = parseFloat(bmiValue.toFixed(2));
 
-}
+      if (this.bmi < 18.5) {
+        this.category = 'Underweight';
+      } else if (this.bmi >= 18.5 && this.bmi < 24.9) {
+        this.category = 'Normal weight';
+      } else if (this.bmi >= 25 && this.bmi < 29.9) {
+        this.category = 'Overweight';
+      } else {
+        this.category = 'Obesity';
+      }
+    } else {
+      alert('Please fill in both fields.');
+    }
+  }
 }
